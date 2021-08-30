9a09fbe507715914ae9d8c11cd6b26c2aee0d82c
sivasrlingala@gmail.com <sivasrlingala@gmail.com>
2021-08-31 01:28:44 +0530

    LinearSearch Python

---
+++ linearsearch.py
@@ -0,0 +1,11 @@
+arr = [2,23,43,22,100,108,554,556]
+n = 108
+c = 0
+for i in arr:
+    if (i == n):
+        print (" This algorithm ran the loop for ", c ," times before finding the element in the array size of ", len(arr))
+        break
+    c+=1
+
+
+    
